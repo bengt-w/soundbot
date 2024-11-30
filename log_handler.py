b481ba524f8d6f8f96c3417846cee7ea4958cd13
Bengt <bengt.wenzel@outlook.com>
2024-11-30 20:16:51 +0100

    oopsie

---
+++ log_handler.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import os
+
+time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+try:
+    os.mkdir("logs")
+except FileExistsError:
+    pass
+
+LOG_FILE = os.path.join("logs", f"log-{datetime.now().strftime("%m-%d-%Y_%H:%M:%S")}.txt")
+LATEST_LOG_FILE = os.path.join("logs", "latest.txt")
+
+open(LATEST_LOG_FILE, 'a').close()
+
+def log(message, level="INFO", time = datetime.now().strftime("%H:%M:%S"), location="WEBUI", user="Unknown", method=None):
+    if not message:
+        raise ValueError("Message cannot be empty")
+    if method != None:
+        method = f"{method} "
+    with open(LATEST_LOG_FILE, "a") as f:
+        f.writelines(f"[{time} {level}] {user}@{location}: {method}{message}\n")
+        
