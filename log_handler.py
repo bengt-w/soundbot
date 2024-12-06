f1e6450b4195311a4e5b25c9b2e14801a08585a9
Bengt <bengt.wenzel@outlook.com>
2024-12-06 20:40:39 +0100

    fixed logs allways showing the same time

--- log_handler.py
+++ log_handler.py
@@ -12,11 +12,13 @@ LATEST_LOG_FILE = os.path.join("logs", "latest.txt")
 
 open(LATEST_LOG_FILE, 'a').close()
 
-def log(message, level="INFO", time = datetime.now().strftime("%H:%M:%S"), location="WEBUI", user="Unknown", method=" "):
-    if not message:
-        raise ValueError("Message cannot be empty")
-    if method != " ":
-        method = f" {method} "
-    with open(LATEST_LOG_FILE, "a") as f:
-        f.writelines(f"[{time} {level}] {user}@{location}:{method}{message}\n")
+def log(message, level="INFO", time = None, location="WEBUI", user="Unknown", method=" "):
+    if time is None:
+        time = datetime.now().strftime("%H:%M:%S")
+        if not message:
+            raise ValueError("Message cannot be empty")
+        if method != " ":
+            method = f" {method} "
+        with open(LATEST_LOG_FILE, "a") as f:
+            f.writelines(f"[{time} {level}] {user}@{location}:{method}{message}\n")
         
